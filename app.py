5b341f473b9e44bbdd8b62d5256a3a897101dd89
Robert Fus <rfus@unc.edu>
2022-11-21 12:41:42 -0500

    MASSIVE efficiency upgrades.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 
 app: Flask = Flask(__name__)
-file_name: str
 
 @app.route("/")
 def index():
@@ -13,55 +12,14 @@ def subject_selector():
 
 @app.route('/team-selector', methods = ["GET", "POST"])
 def puller():
-    global file_name
-    
     if request.method == "POST":
         team: str = request.form['team']
         year: str = request.form['year']
-        if team == "arsenal":
-            if year == "2022-23":
-                return render_template("arsenal22-23.html")
-                # TODO: Update the HTML path for the specific image
-                # file_name = "arsenal22-23"
-            if year == "2021-22":
-                return render_template("arsenal21-22.html")
-                # TODO: Update the HTML path for the specific image
-                # b: str = "arsenal21-22"
-            if year == "2020-21":
-                return render_template("arsenal20-21.html")
-                # TODO: Update the HTML path for the specific image
-                # c: str = "arsenal20-21"
-            if year == "2019-20":
-                return render_template("arsenal19-20.html")
-                # TODO: Update the HTML path for the specific image
-                # d: str = "arsenal19-20"
-        if team == "chelsea":
-            if year == "2022-23":
-                return render_template("chelsea22-23.html")
-            if year == "2021-22":
-                return render_template("chelsea21-22.html")
-            if year == "2020-21":
-                return render_template("chelsea20-21.html")
-            if year == "2019-20":
-                return render_template("chelsea19-20.html")
-        if team == "liverpool":
-            if year == "2022-23":
-                return render_template("liverpool22-23.html")
-            if year == "2021-22":
-                return render_template("liverpool21-22.html")
-            if year == "2020-21":
-                return render_template("liverpool20-21.html")
-            if year == "2019-20":
-                return render_template("liverpool19-20.html")
-        if team == "manchester_city":
-            if year == "2022-23":
-                return render_template("manchester_city22-23.html")
-            if year == "2021-22":
-                return render_template("manchester_city21-22.html")
-            if year == "2020-21":
-                return render_template("manchester_city20-21.html")
-            if year == "2019-20":
-                return render_template("manchester_city19-20.html")
+        file_name: str = team + year
+        team_data_file = open(f"static/team_data/{file_name}", "r")
+        data: str = team_data_file.read()
+        team_data_file.close()
+        return render_template("image.html", file_name = file_name, team_data = data)
     return render_template("team-selector.html")
 
 if __name__ == '__main__':
